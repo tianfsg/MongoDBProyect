3d4b2cab25af598872e6dbed5d3529513a79df39
Willyfsg <sebastian.gutierrez@live.u-tad.com>
2021-10-16 16:19:46 +0200

    metodos y cursor listo

--- limpio.py
+++ limpio.py
@@ -1,5 +1,6 @@
-__author__ = 'Nombres_y_Apellidos'
+__author__ = 'Hao_Long_y_Sebastian_Gutierrez'
 
+import pymongo
 from pymongo import MongoClient
 
 def getCityGeoJSON(address):
@@ -36,14 +37,18 @@ class ModelCursor:
     def next(self):
         """ Devuelve el siguiente documento en forma de modelo
         """
-        #TODO
-        pass #No olvidar eliminar esta linea una vez implementado
+        if ModelCursor.alive:
+            return self.model_class(**self.command_cursor.next())
 
     @property
     def alive(self):
         """True si existen más modelos por devolver, False en caso contrario
         """
-        return self.command_cursor.hasNext()
+        try:
+            self.command_cursor.next()
+            return True
+        except:
+            return False
 
 class Persona:
     """ Prototipo de la clase modelo
@@ -56,54 +61,103 @@ class Persona:
     admissible_vars = []
     db = None
 
-    def __init__(self, **kwargs):
-        lista = list(kwargs.keys())
-        cont = 0
+    def __init__(self, **kwargs):  
 
+        id_aux = None
+        if '_id' in kwargs:
+            id_aux = kwargs['_id']
+            kwargs.pop('_id')
+
+        cont = 0
         for x in self.required_vars:
-            if x == lista(x):
-                cont +=1
-                break
+            for i in kwargs:
+                if x == i:
+                    cont +=1
 
         if cont == len(self.required_vars):
             all_vars = self.required_vars + self.admissible_vars
             for x in kwargs:
-                var_flag = False
-                if all_vars[x] == lista[x]:
-                    var_flag = True
-                    break
+                for i in all_vars:
+                    var_flag = False
+                    if x == i :
+                        var_flag = True
+                        break
             if var_flag == False:
-                raise Exception ('La key: ', lista[x], ' NO ES VALIDA.')
+                raise Exception ('La key: ',x,' NO ES VALIDA.')
 
+        if id_aux != None:
+            self._id = id_aux
         self.__dict__.update(kwargs)
 
 
     def save(self):
-        pass
+        try:
+            if hasattr(self, '_id'): #Significa que existe en la BD
+                values = {'$set': self.mod}
+                self.db.persona.update_one({'_id': self._id.inserted_id}, values) #DEBERIA SER EL NIF.
+                print("\nSe ha actualizado correctamente.")
+            else: #No existe en la BD
+                self._id = self.db.persona.insert_one(self.__dict__)
+                print('Se ha registrado correctamente.')
+        except:
+            print('\n\nERR: Algo ha fallado en Persona.save()')
 
     def set(self, **kwargs):
-        #TODO
-        pass #No olvidar eliminar esta linea una vez implementado
-    
+
+        id_aux = None
+        if hasattr(self, '_id'):
+            id_aux = self.__dict__['_id']
+            self.__dict__.pop('_id')
+
+        curr = list(self.__dict__.keys())
+        mod = list(kwargs.keys())
+
+        #all_vars = self.required_vars + self.admissible_vars
+        var_flag = False
+        for x in curr:
+            for i in mod:
+                if x == i:
+                    var_flag = True
+                    break
+        if var_flag == False:
+            raise Exception ('La key: ', i, ' NO ES VALIDA.')
+
+        for x in curr:
+            for i in mod:
+                if x == i:
+                    self.__dict__[x] = kwargs[i]
+                    self.mod = {x: kwargs[i]}
+                    
+        if id_aux != None:
+            self._id = id_aux
+ 
     @classmethod
     def find(cls, filter):
         """ Devuelve un cursor de modelos        
         """ 
-        #TODO
-        # cls es el puntero a la clase
-        pass #No olvidar eliminar esta linea una vez implementado
+        return ModelCursor(Persona, Persona.db.persona.find(filter))
 
     @classmethod
-    def init_class(cls, db, vars_path="model_name.vars"):
+    def init_class(cls, db, vars_path="persona_vars.txt"):
         """ Inicializa las variables de clase en la inicializacion del sistema.
         Argumentos:
             db (MongoClient) -- Conexion a la base de datos.
             vars_path (str) -- ruta al archivo con la definicion de variables
             del modelo.
         """
-        #TODO
-        # cls es el puntero a la clase
-        pass #No olvidar eliminar esta linea una vez implementado
+        try:
+            file = open(vars_path, 'r')
+            content_file = file.read()
+            nothing = content_file.split('\n')
+            required_vars = nothing[0].split(',')
+            admissible_vars = nothing[1].split(',')
+            file.close()
+        except:
+            print('el fichero de vars no existe')
+
+        Persona.required_vars = required_vars
+        Persona.admissible_vars = admissible_vars
+        Persona.db = db
 
 
 # Q1: Listado de todas las compras de un cliente
@@ -113,6 +167,20 @@ Q1 = []
 # Q2: etc...
 
 if __name__ == '__main__':
-    #TODO
-    pass #No olvidar eliminar esta linea una vez implementado
+    client = MongoClient('localhost', 27017)
+    Persona.init_class(client['mongoproyect'])
+
+    #TODO FALTA CREAR UNIQUE KEY AS _NIF && GeoJSON 
+    x = {'nombre': 'Sebas', 'apellido': 'Guti', 'telefono': '655408703','nif': 'y7502011t'}
+
+    p1 = Persona(**x)
+    p1.save()
+    p1.set(**{'telefono':'5000000'})
+    p1.save()
+    cursor = Persona.find({'nombre': 'Sebas'})
+    print(cursor.next())
+
+
+
+    
 
